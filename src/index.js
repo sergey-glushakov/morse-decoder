3fbea34d87151b09d938c1974ebe55a50c60bd03
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2022-01-22 15:44:47 +0300

    feat: morse-decoder task finish

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arrString = expr.match(/.{1,10}/g);
+    let newArr = arrString.map(function (item) {
+        let index = item.indexOf('1');
+        let newItem = item.slice(index, item.length);
+        let arr = newItem.match(/.{1,2}/g);
+        let newArr = arr.map(function (item) {
+            if(item === '10') return '.';
+            if(item === '11') return '-';
+            if(item === '*') return ' ';
+        })
+        let string = newArr.join('');
+        let char = MORSE_TABLE[string];
+        return char;
+        
+    })
+
+    newArr = newArr.map(function (item) {
+        if (item === undefined) {
+            return ' ';
+        } else {
+            return item;
+        }
+    })
+    let result = newArr.join('');
+    return result;
 }
 
 module.exports = {
